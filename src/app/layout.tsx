7864007fed5a04f13ff474395411b583aac2acee
Marc España Quinquilla <meq.1515@gmail.com>
2026-03-22 18:22:43 +0100

    trying to fix insta styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata, Viewport } from "next";
+import Script from "next/script";
 import { headers } from "next/headers";
 import { inter, fraunces } from "@/lib/fonts";
 import "./globals.css";
@@ -31,6 +32,9 @@ export default async function RootLayout({
         />
       </head>
       <body className={`${inter.variable} ${fraunces.variable} antialiased`}>
+        <Script id="inapp-browser-detect" strategy="beforeInteractive">
+          {`(function(){try{var u=navigator.userAgent||"";if(/Instagram|FBAN|FBAV|FB_IAB|FBIOS/i.test(u))document.documentElement.setAttribute("data-inapp-browser","1");}catch(e){}})();`}
+        </Script>
         {children}
       </body>
     </html>
