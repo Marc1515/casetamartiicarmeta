ee58acc8e84d4571e95f3f854db98eedc5824261
Marc España Quinquilla <meq.1515@gmail.com>
2026-04-01 18:59:56 +0200

    Banners img changed

--- src/components/public/Banner1.tsx
+++ src/components/public/Banner1.tsx
@@ -13,7 +13,7 @@ export default function Banner1() {
       className="relative w-full min-h-screen"
     >
       {/* Imagen de fondo fija a pantalla completa */}
-      <div className="absolute inset-0 bg-[url('/img/sunset.avif')] bg-cover bg-center bg-fixed" />
+      <div className="absolute inset-0 bg-[url('/img/sunset.png')] bg-cover bg-center bg-fixed" />
 
       {/* Capa que aparece al hacer scroll */}
       <ScrollReveal className="relative z-10 flex min-h-screen w-full items-center justify-center bg-black/20">
--- src/components/public/Banner2.tsx
+++ src/components/public/Banner2.tsx
@@ -13,7 +13,7 @@ export default function Banner2() {
       className="relative w-full min-h-screen"
     >
       {/* Imagen de fondo fija a pantalla completa */}
-      <div className="absolute inset-0 bg-[url('/img/faroFangar.avif')] bg-cover bg-center bg-fixed" />
+      <div className="absolute inset-0 bg-[url('/img/loFaro.png')] bg-cover bg-center bg-fixed" />
 
       {/* Capa que aparece al hacer scroll */}
       <ScrollReveal className="relative z-10 flex min-h-screen w-full items-center justify-center bg-black/20">
