becf9ad291750ef559e77720f5ee903990075b7d
Marc España Quinquilla <meq.1515@gmail.com>
2026-04-27 09:51:12 +0200

    refactor: adjust layout and spacing in CalendarInfo component for improved readability and consistency

--- src/modules/public-site/presentation/ui/calendar/CalendarInfo.tsx
+++ src/modules/public-site/presentation/ui/calendar/CalendarInfo.tsx
@@ -4,15 +4,15 @@ export default function CalendarInfo() {
   const t = useTranslations("calendar.info");
 
   return (
-    <div className="flex flex-col gap-4 mt-16 min-w-0 xl:col-span-1 text-sm leading-snug text-[#393E46]">
+    <div className="flex flex-col gap-4 mt-10 min-w-0 xl:col-span-1 text-sm leading-snug text-[#393E46]">
       <p>{t("intro")}</p>
       <p>{t("intro2")}</p>
-      <div>
+      <div className="flex flex-col gap-2">
         <p className="font-semibold">{t("highSeasonTitle")}</p>
         <p>{t("highSeasonText")}</p>
       </div>
 
-      <div>
+      <div className="flex flex-col gap-2">
         <p className="font-semibold">{t("additionalInfoTitle")}</p>
         <p>{t("additionalInfoText")}</p>
       </div>
