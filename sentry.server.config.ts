31727536794a1ac1e0e4fea83f9b4bf68b149f84
Marc1515 <meq.1515@gmail.com>
2026-05-20 20:18:01 +0100

    chore: clean up Sentry configuration files by removing comments and adjusting replaysSessionSampleRate for better performance in production

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -1,19 +1,11 @@
-// This file configures the initialization of Sentry on the server.
-// The config you add here will be used whenever the server handles a request.
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/
-
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
   dsn: "https://63caa7658aef638c94964b3d025fac18@o4511422770708480.ingest.de.sentry.io/4511422854398032",
 
-  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: process.env.NODE_ENV === "production" ? 0.1 : 1,
 
-  // Enable logs to be sent to Sentry
   enableLogs: process.env.NODE_ENV !== "development",
 
-  // Enable sending user PII (Personally Identifiable Information)
-  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
   sendDefaultPii: false,
 });
